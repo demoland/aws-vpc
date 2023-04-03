de126c12dbdf886924518d6b56614ace1d255124
Dan Fedick <daniel.fedick@hashicorp.com>
2023-04-03 17:08:42 -0400

    Add management and ssh sg to aws-vpc

--- main.tf
+++ main.tf
@@ -29,3 +29,34 @@ module "vpc" {
   }
 
 }
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_key_pair" "management_key" {
+  key_name   = "management"
+  public_key = var.management_pubkey
+}
